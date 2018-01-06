5bb0c2e5d615ce1e8e3a4ae0aa2a35fca8544c50
FIRETRUCK\Linda <linda.freienthal96@gmail.com>
2018-01-06 13:47:59 +0200

    Viimased muudatused.

--- src/main/scala/intro/SalapäranePilt.scala
+++ src/main/scala/intro/SalapäranePilt.scala
@@ -3,20 +3,35 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.all._
 import org.scalajs.dom._
 object SalapäranePilt {
+  val r = scala.util.Random
+  var random: Int = r.nextInt(28)
+  var pakutud = 0
 
-  case class State(y: Int) {
-    val r = scala.util.Random
-    var random: Int = r.nextInt(28)
-    while (random == 0){
-      random = r.nextInt(28)
-    }
+  case class State(var y: Int) {
     def jura: String = {
       if (y == random){
         "Õige! Ta on võllas kõlkunud " + random + " korda."
       }
-      "Vale. Proovi veel!"
+      else {
+        if (pakutud == 0){
+          pakutud += 1
+          "Ootan pakkumist."
+        }
+        else if (pakutud == 1){
+          pakutud += 1
+          "Hmmm....Pole päris täpselt see. Võid veel kaks korda pakkuda."
+        }
+        else if (pakutud == 2){
+          pakutud += 1
+          "Nii keeruline see ennustamine ka ei ole. Paku veel üks kord."
+        }
+        else {
+          "Vale. Õige oli: " + random + ". Uueks äraarvamiseks palun refreshi veebilehte."
+        }
+      }
     }
   }
+
 
 
   class Backend($: BackendScope[Unit, State]) {
@@ -30,7 +45,9 @@ object SalapäranePilt {
     def render(statee : State) =
       div(
         input.text(value := statee.y, onChange ==> handler(updateY)),
-        s"${statee.jura}"
+        button("Suurenda pakkumist ühe võrra", onClick --> updateY.modState(_+1)),
+        br,
+        s"${statee.jura}",
       )
   }
 
